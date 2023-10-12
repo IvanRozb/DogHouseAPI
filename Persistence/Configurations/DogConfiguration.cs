e30e46b2d49bce6ca3d8e8f10924d5d5fc3f47e6
IvanRozb <ivanrozb4225@gmail.com>
2023-10-13 01:29:40 +0300

    Added Dog Configuration

---
+++ Persistence/Configurations/DogConfiguration.cs
@@ -0,0 +1,28 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations;
+
+internal sealed class DogConfiguration : IEntityTypeConfiguration<Dog>
+{
+    public void Configure(EntityTypeBuilder<Dog> builder)
+    {
+        builder.ToTable("dogs");
+
+        builder.HasKey(dog => dog.Id);
+
+        builder.Property(dog => dog.Id).ValueGeneratedOnAdd();
+
+        builder.Property(dog => dog.Name).HasMaxLength(50);
+        builder.Property(dog => dog.Name).IsRequired();
+        
+        builder.Property(dog => dog.Color).HasMaxLength(50);
+        builder.Property(dog => dog.Color).IsRequired();
+        
+        builder.Property(dog => dog.TailLength).IsRequired();
+        
+        builder.Property(dog => dog.Weight).IsRequired();
+        
+    }
+}
