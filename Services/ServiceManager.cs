e6d3fb16173ba5eb733d10b1e5ee2c0ba429acfd
IvanRozb <ivanrozb4225@gmail.com>
2023-10-13 02:45:42 +0300

    Added ServiceManager

---
+++ Services/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Repositories;
+using Services.Abstractions;
+
+namespace Services
+{
+    public sealed class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IDogsService> _lazyDogsService;
+
+        public ServiceManager(IRepositoryManager repositoryManager)
+        {
+            _lazyDogsService = new Lazy<IDogsService>(() => 
+                new DogsService(repositoryManager));
+        }
+
+        public IDogsService DogsService => _lazyDogsService.Value;
+    }
+}
