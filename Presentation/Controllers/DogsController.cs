a53989580e106336eb3f8a97f05389d57fb72c38
IvanRozb <ivanrozb4225@gmail.com>
2023-10-14 20:28:49 +0300

    Added GetDogsAsync validation

--- Presentation/Controllers/DogsController.cs
+++ Presentation/Controllers/DogsController.cs
@@ -21,6 +21,34 @@ public class DogsController : ControllerBase
         [FromQuery] int? pageSize,
         CancellationToken cancellationToken = default)
     {
+        if (attribute is not null)
+        {
+            var availableAttributes = new List<string> { "name", "color", "tail_length", "weight" };
+            if (!availableAttributes.Contains(attribute))
+            {
+                throw new BadRequestException("attribute must be name, color, tail_length, or weight");
+            }
+        }
+        
+        if (order is not null)
+        {
+            var availableOrders = new List<string> { "asc", "desc"};
+            if (!availableOrders.Contains(order))
+            {
+                throw new BadRequestException("order must be asc or desc");
+            }
+        }
+        
+        if (pageNumber < 1)
+        {
+            throw new BadRequestException("pageNumber must be higher then 0");
+        }
+        
+        if (pageSize < 1)
+        {
+            throw new BadRequestException("pageSize must be higher then 0");
+        }
+        
         var dogs = await _serviceManager.DogsService.GetAllAsync(attribute, order, pageNumber, pageSize, cancellationToken);
         return Ok(dogs);
     }
