e66ac68e34bbe4a25a568ce89b6296eab055bb80
IvanRozb <ivanrozb4225@gmail.com>
2023-10-14 19:28:20 +0300

    Added validation to Dog entity fields

--- Domain/Entities/Dog.cs
+++ Domain/Entities/Dog.cs
@@ -1,9 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
 namespace Domain.Entities;
 
 public class Dog
 {
+    [Required(ErrorMessage = "name is required.")]
+    [MaxLength(50, ErrorMessage = "name must be at most 50 characters.")]
     public string Name { get; set; } = "";
+
+    [Required(ErrorMessage = "color is required.")]
+    [MaxLength(50, ErrorMessage = "color must be at most 50 characters.")]
     public string Color { get; set; } = "";
-    public int TailLength { get; set; }
-    public int Weight { get; set; }
+
+    [Required(ErrorMessage = "tail_length is required.")]
+    [Range(0.01, 1000, ErrorMessage = "tail_length must be a number between 0.01 and 1000.")]
+    [JsonProperty("tail_length")] 
+    public double TailLength { get; set; }
+
+    [Required(ErrorMessage = "weight is required.")]
+    [Range(0.01, 1000, ErrorMessage = "weight must be a number between 0.01 and 1000.")]
+    public double Weight { get; set; }
 }
--- Persistence/Configurations/DogConfiguration.cs
+++ Persistence/Configurations/DogConfiguration.cs
@@ -9,20 +9,6 @@ internal sealed class DogConfiguration : IEntityTypeConfiguration<Dog>
     public void Configure(EntityTypeBuilder<Dog> builder)
     {
         builder.ToTable("dogs");
-
         builder.HasKey(dog => dog.Name);
-
-        builder.Property(dog => dog.Name).ValueGeneratedOnAdd();
-
-        builder.Property(dog => dog.Name).HasMaxLength(50);
-        builder.Property(dog => dog.Name).IsRequired();
-        
-        builder.Property(dog => dog.Color).HasMaxLength(50);
-        builder.Property(dog => dog.Color).IsRequired();
-        
-        builder.Property(dog => dog.TailLength).IsRequired();
-        
-        builder.Property(dog => dog.Weight).IsRequired();
-        
     }
 }
