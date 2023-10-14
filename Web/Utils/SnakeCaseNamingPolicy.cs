e6d164089c8027b576f42dfa94ddd7b9b55f2f61
IvanRozb <ivanrozb4225@gmail.com>
2023-10-14 20:29:33 +0300

    Removed unnecessary comments

--- Web/Utils/SnakeCaseNamingPolicy.cs
+++ Web/Utils/SnakeCaseNamingPolicy.cs
@@ -8,7 +8,6 @@ public class SnakeCaseNamingPolicy : JsonNamingPolicy
 
     public override string ConvertName(string name)
     {
-        // Conversion to other naming convention goes here. Like SnakeCase, KebabCase etc.
         return name.ToSnakeCase();
     }
 }
