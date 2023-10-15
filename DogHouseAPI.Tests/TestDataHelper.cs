28e4bf157159202c49d8ad2cb700c41b89d6e805
IvanRozb <ivanrozb4225@gmail.com>
2023-10-15 18:58:26 +0300

    Created DogsRepositoryTests

---
+++ DogHouseAPI.Tests/TestDataHelper.cs
@@ -0,0 +1,16 @@
+using Domain.Entities;
+
+namespace DogHouseAPI.Tests;
+
+public static class TestDataHelper
+{
+    public static IEnumerable<Dog> GetFakeDogsList()
+    {
+        return new List<Dog>
+        {
+            new() { Name = "Dog1", Color = "Brown", TailLength = 10, Weight = 20 },
+            new() { Name = "Dog3", Color = "White", TailLength = 8, Weight = 18 },
+            new() { Name = "Dog2", Color = "Black", TailLength = 12, Weight = 25 },
+        };
+    }
+}
