87907c1b17767f633d59971a4bb864cd7e4f1685
IvanRozb <ivanrozb4225@gmail.com>
2023-10-15 18:10:33 +0300

    Added parameterless constructor in RepositoryDbContext for testing

--- Persistence/RepositoryDbContext.cs
+++ Persistence/RepositoryDbContext.cs
@@ -4,15 +4,19 @@ using Microsoft.Extensions.Configuration;
 
 namespace Persistence;
 
-public sealed class RepositoryDbContext : DbContext
+public class RepositoryDbContext : DbContext
 {    
+    public RepositoryDbContext()
+    {
+    }
+
     public RepositoryDbContext(
         DbContextOptions<RepositoryDbContext> options)
         : base(options)
     {
     }
 
-    public DbSet<Dog> Dogs { get; set; }
+    public virtual DbSet<Dog> Dogs { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) =>
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(RepositoryDbContext).Assembly);
